0b8b48299826f6fa0776346a14eaccdf9c6d8e96
elias-sweed <slipknotcoreytaylor420@gmail.com>
2026-09-04 09:39:40 -0500

    Fix: Delete import innecesar

--- src/pages/admin/Jurados.tsx
+++ src/pages/admin/Jurados.tsx
@@ -1,18 +1,15 @@
-import { useMemo, useRef, useState } from 'react'
+import { useMemo, useState } from 'react'
 import PanelHeader from '../../components/admin/PanelHeader'
 import Section from '../../components/admin/Section'
 import {
   IconoCheck,
   IconoLapiz,
   IconoPapelera,
-  IconoDescargar,
-  IconoImportar,
 } from '../../components/admin/Iconos'
 import { usePanelData } from '../../context/PanelDataContext'
 import { getSupabase } from '../../lib/supabase'
 import { generarCodigoJurado, generarTokenAcceso } from '../../utils/codigos'
 import { logConsulta, logError } from '../../utils/devlog'
-import { descargarExcel } from '../../utils/exportExcel'
 
 export default function Jurados() {
   const { jurados, cargandoInicial, recargar } = usePanelData()
